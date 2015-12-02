6669282580a0e8d38a571bf1bc78a777c3542e11
jjc12 <jjc11j@my.fsu.edu>
2015-12-01 21:13:55 -0500

    changes

--- Database-Theory-Project/myproject/WebApp/static/script/preload.js
+++ Database-Theory-Project/myproject/WebApp/static/script/preload.js
@@ -1,7 +1,11 @@
 // Adobe ImageReady Preload Script
 // modified, of course, to allow for a base directory
 
-var baseDir = "/images/";
+var baseDir = "../static/images/";
+
+$(document).ready(function() {
+    $('#example').DataTable();
+} );
 
 function newImage(arg) {
 	if (document.images) {
